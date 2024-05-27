dfadd7d8caa4fd3b517b0b22233da5a2fcd7e579
223510892 - Nadia Fadila <164306649+nadiafaadila@users.noreply.github.com>
2024-05-27 11:21:13 +0700

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,3 @@
-<!-- App.vue -->
 <template>
   <div id="app">
     <div class="header">
@@ -8,8 +7,25 @@
       </ul>
     </div>
     <div class="container">
-      <todo-list v-if="selectedMenu === 'Todos'" :todo-items="ToDoItems" @todo-added="addToDo" @update-done-status="updateDoneStatus" @delete-todo="deleteToDo" @edit-todo="editToDo" @filter="filterToDo"></todo-list>
-      <post-list v-if="selectedMenu === 'Post'" :users="users" :user-posts="userPosts"></post-list>
+      <todo-list v-if="selectedMenu === 'Todos'" :todo-items="ToDoItems">
+        <template v-slot:todo-item="props">
+          <to-do-item
+            :label="props.item.label"
+            :done="props.item.done"
+            :id="props.item.id"
+            @checkbox-changed="updateDoneStatus(props.item.id)"
+            @item-deleted="deleteToDo(props.item.id)"
+            @item-edited="editToDo(props.item.id, $event)"
+          ></to-do-item>
+        </template>
+      </todo-list>
+      <post-list v-if="selectedMenu === 'Post'" :users="users" :user-posts="userPosts">
+        <template v-slot:post-item="props">
+          <li :key="props.post.id">
+            {{ props.post.title }}
+          </li>
+        </template>
+      </post-list>
     </div>
   </div>
 </template>
@@ -17,7 +33,6 @@
 <script>
 import ToDoList from "./components/TodoList.vue";
 import PostList from "./components/PostList.vue";
-import uniqueId from "lodash.uniqueid";
 
 export default {
   name: "App",
@@ -25,36 +40,6 @@ export default {
     ToDoList,
     PostList,
   },
-  data() {
-    return {
-      selectedMenu: 'Todos', // Default menu selection
-      ToDoItems: [],
-      users: [], // Placeholder for users data
-      userPosts: [] // Placeholder for user posts data
-    };
-  },
-  methods: {
-    addToDo(toDoLabel) {
-      this.ToDoItems.push({
-        id: uniqueId("todo-"),
-        label: toDoLabel,
-        done: false,
-      });
-    },
-    updateDoneStatus(toDoId) {
-      const toDoToUpdate = this.ToDoItems.find((item) => item.id === toDoId);
-      toDoToUpdate.done = !toDoToUpdate.done;
-    },
-    deleteToDo(toDoId) {
-      const itemIndex = this.ToDoItems.findIndex((item) => item.id === toDoId);
-      this.ToDoItems.splice(itemIndex, 1);
-    },
-    editToDo(toDoId, newLabel) {
-      const toDoToEdit = this.ToDoItems.find((item) => item.id === toDoId);
-      toDoToEdit.label = newLabel;
-    },
-    filterToDo(type) {
-      // Handle filtering
-    },
-    fetchUsers() {
-      fetch('https://jsonplaceholder.typicode.com
+  // rest of the code...
+};
+</script>
